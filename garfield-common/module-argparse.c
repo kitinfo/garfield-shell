241d9fa3942c4052de3b4d4112a786dd8d760a27
cbdev <cb@cbcdn.com>
2013-12-11 10:01:20 +0100

    moved main argument parsing for modules to garfield-common

---
+++ garfield-common/module-argparse.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "getlogin.h"
+
+#include "pqconnect.c"
+#include "getpass.c"
+
+#define MAX_PASS_LEN 254
+#define MAX_USER_LEN 254
+
+int main(int argc, char** argv){
+	int i,c;
+	
+	PGconn* conn;
+	
+	char user[MAX_USER_LEN+1];
+	*user=0;
+	char pass[MAX_PASS_LEN+1];
+	*pass=0;
+	char* server="fsmi-db.fsmi.uni-karlsruhe.de";
+	char* port="5432"; //garfield test instance is on 5435
+	char* dbname="garfield";
+	char* mode=NULL;
+	bool use_pgpass=false;
+	int mode_arg=-1;
+	
+	//parse arguments
+	for(i=1;i<argc;i++){
+		if(*argv[i]=='-'){
+			if(i+1<argc&&!strcmp(argv[i],"--user")){
+				strncpy(user,argv[i+1],MAX_USER_LEN);
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--pass")){
+				strncpy(pass,argv[i+1],MAX_PASS_LEN);
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--server")){
+				server=argv[i+1];
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--port")){
+				port=argv[i+1];
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--dbname")){
+				dbname=argv[i+1];
+				i++;
+			}
+			else if(!strcmp(argv[i],"--verbose")){
+				beVerbose=true;
+			}
+			else if(!strcmp(argv[i],"--pgpass")){
+				use_pgpass=true;
+			}
+		}
+		else{
+			if(!mode){
+				mode=argv[i];
+				mode_arg=i;
+			}
+		}
+	}
+	
+	if(!mode){
+		printf("No operation specified\n");
+		exit(usage(argv[0]));
+	}
+	
+	//if no user supplied, get from environment
+	if(*user==0){
+		i=getlogin_r(user,MAX_USER_LEN);
+		if(i!=0){
+			printf("Failed to get user name (Return value %d, errno %d), try --user\n\n",i,errno);
+			exit(usage(argv[0]));
+		}
+	}
+	
+	if(beVerbose){
+		printf("Connecting to %s:%s\n",server,port);
+	}
+	
+	//if none supplied, ask for password
+	if(*pass==0&&!use_pgpass){
+		printf("Enter Garfield / DB access password for %s: ",user);
+		
+		if(ask_password(pass,MAX_PASS_LEN)<0){
+			printf("Aborting\n");
+			exit(1);
+		}
+		
+		printf("\n\n");
+	}
+	
+	conn=database_connect(server, port, dbname, user, use_pgpass?NULL:pass, beVerbose);
+	if(!conn){
+		return -1;
+	}
+	
+	i=module_main(conn,argc-mode_arg,argv+mode_arg);
+	
+	if(beVerbose){
+		printf("Module finished with result %d\n",i);
+	}
+	PQfinish(conn);
+	return i;
+}
--- garfield-snack/garfield-snack.c
+++ garfield-snack/garfield-snack.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
 #include <malloc.h>
+#include <stdbool.h>
+#include <libpq-fe.h>
 
 #ifndef _WIN32
 	#include <arpa/inet.h>
 #else
 	#include <winsock2.h>
 #endif
-
-#include "../garfield-common/getlogin.h"
-
-bool beVerbose=false;
-
-
-#include "../garfield-common/pqconnect.c"
-#include "../garfield-common/getpass.c"
-
-//operation modes
-#include "mode_stats.c"
-#include "mode_find.c"
-#include "mode_buy.c"	//buy needs find
-
-#define MAX_PASS_LEN 254
-#define MAX_USER_LEN 254
 
 int usage(char* fn){
 	printf("garfield-snack shell utility\n");
@@ -59,113 +44,30 @@ int usage(char* fn){
 	return -1;
 }
 
-int main(int argc, char** argv){
-	int i,c;
-	
-	PGconn* conn;
-	
-	char user[MAX_USER_LEN+1];
-	*user=0;
-	char pass[MAX_PASS_LEN+1];
-	*pass=0;
-	char* server="fsmi-db.fsmi.uni-karlsruhe.de";
-	char* port="5432"; //garfield test instance is on 5435
-	char* dbname="garfield";
-	char* mode=NULL;
-	bool use_pgpass=false;
-	int mode_arg=-1;
-	
-	//parse arguments
-	for(i=1;i<argc;i++){
-		if(*argv[i]=='-'){
-			if(i+1<argc&&!strcmp(argv[i],"--user")){
-				strncpy(user,argv[i+1],MAX_USER_LEN);
-				i++;
-			}
-			else if(i+1<argc&&!strcmp(argv[i],"--pass")){
-				strncpy(pass,argv[i+1],MAX_PASS_LEN);
-				i++;
-			}
-			else if(i+1<argc&&!strcmp(argv[i],"--server")){
-				server=argv[i+1];
-				i++;
-			}
-			else if(i+1<argc&&!strcmp(argv[i],"--port")){
-				port=argv[i+1];
-				i++;
-			}
-			else if(i+1<argc&&!strcmp(argv[i],"--dbname")){
-				dbname=argv[i+1];
-				i++;
-			}
-			else if(!strcmp(argv[i],"--verbose")){
-				beVerbose=true;
-			}
-			else if(!strcmp(argv[i],"--pgpass")){
-				use_pgpass=true;
-			}
-		}
-		else{
-			if(!mode){
-				mode=argv[i];
-				mode_arg=i;
-			}
-		}
+bool beVerbose=false;
+
+//operation modes
+#include "mode_stats.c"
+#include "mode_find.c"
+#include "mode_buy.c"	//buy needs find
+
+int module_main(PGconn* db, int argc, char** argv){
+	int i;
+	//select garfield-snack mode
+	if(!strcmp(argv[0],"buy")){
+		i=mode_buy(db,argc,argv);
 	}
-	
-	if(!mode){
-		printf("No operation specified\n");
-		exit(usage(argv[0]));
+	else if(!strcmp(argv[0],"find")||!strcmp(argv[0],"search")){
+		i=mode_find(db,argc,argv);
 	}
-	
-	//if no user supplied, get from environment
-	if(*user==0){
-		i=getlogin_r(user,MAX_USER_LEN);
-		if(i!=0){
-			printf("Failed to get user name (Return value %d, errno %d), try --user\n\n",i,errno);
-			exit(usage(argv[0]));
-		}
-	}
-	
-	if(beVerbose){
-		printf("Connecting to %s:%s\n",server,port);
-	}
-	
-	//if none supplied, ask for password
-	if(*pass==0&&!use_pgpass){
-		printf("Enter Garfield / DB access password for %s: ",user);
-		
-		if(ask_password(pass,MAX_PASS_LEN)<0){
-			printf("Aborting\n");
-			exit(1);
-		}
-		
-		printf("\n\n");
-	}
-	
-	conn=database_connect(server, port, dbname, user, use_pgpass?NULL:pass, beVerbose);
-	if(!conn){
-		return -1;
-	}
-	
-	//select mode procedure
-	if(!strcmp(mode,"buy")){
-		i=mode_buy(conn,argc-mode_arg,argv+mode_arg);
-	}
-	else if(!strcmp(mode,"find")||!strcmp(mode,"search")){
-		i=mode_find(conn,argc-mode_arg,argv+mode_arg);
-	}
-	else if(!strcmp(mode,"stats")||!strcmp(mode,"info")){
-		i=mode_stats(conn,argc-mode_arg,argv+mode_arg);
+	else if(!strcmp(argv[0],"stats")||!strcmp(argv[0],"info")){
+		i=mode_stats(db,argc,argv);
 	}
 	else{
-		printf("No such mode \"%s\".\n",mode);
-		i=usage(argv[0]);
+		printf("No such mode \"%s\".\n",argv[0]);
+		i=usage("garfield-snack");
 	}
-	
-	if(beVerbose){
-		printf("Mode %s finished with result %d\n",mode,i);
-	}
-	PQfinish(conn);
 	return i;
 }
+
+#include "../garfield-common/module-argparse.c"
