c074c3b82f4896c0fa7944a08e9ef9242015a761
jan.duepmeier <jan.duepmeier@student.kit.edu>
2014-03-03 23:48:31 +0100

    refactor parsing args in garfield-shell

--- shell/sources/shell_parse.c
+++ shell/sources/shell_parse.c
@@ -1,41 +1,54 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdbool.h>
+#include <string.h>
 #include "../headers/shell_sec.h"
 #include "../headers/shell_debug.h"
 #include "../headers/shell_help.h"
 
-/**
- * @handsoff
- * parses Arguments
- */
 void parseArguments(int argc, char* argv[]) {
-    int c = -1;
-    opterr = 0;
-    while ((c = getopt(argc, argv, "u:p:h:d")) != -1) {
 
-		switch (c) {
-			case 'u':
-			setUser(optarg);
-			break;
-			case 'p':
-			setPassword(optarg);
-			break;
-			case 'h':
-			printHelp();
-			break;
-			case 'd':
-			setDebug(true);
-			break;
-			case '?':
-			printf("Invalid arguments.\n");
-			printHelp();
-			break;
-			default:
-			return;
-			break;
+	//parse arguments
+	int i;
+	for(i=1;i<argc;i++){
+		if(*argv[i]=='-'){
+			if(i+1<argc&&!strcmp(argv[i],"--user")){
+				setUser(argv[i+1]);
+				i++;
+			}
+			if(i+1<argc&&!strcmp(argv[i],"-u")){
+				setUser(argv[i+1]);
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--pass")){
+				setPassword(argv[i+1]);
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"-p")){
+				setPassword(argv[i+1]);
+				i++;
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"--help")){
+				printHelp();
+			}
+			else if(i+1<argc&&!strcmp(argv[i],"-h")){
+				printHelp();
+			}
+			else if(!strcmp(argv[i],"--debug")){
+				setDebug(true);
+			}
+			else if(!strcmp(argv[i],"-d")){
+				setDebug(true);
+			}
+			else if(!strcmp(argv[i],"--pgpass")){
+				//TODO pgpass imp
+			}
+			else if(!strcmp(argv[i],"-g")){
+				//TODO pgpass imp
+			}
+			else {
+				printf("Invalid arguments.\n");
+				printHelp();
+			}
 		}
-
-    }
-
+	}
 }
