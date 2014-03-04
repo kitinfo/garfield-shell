64fbd1685f0f884ca940fac882a480504cdc6c72
jan.duepmeier <jan.duepmeier@student.kit.edu>
2014-03-04 01:00:59 +0100

    new make version for win

--- shell/sources/shell_exec.c
+++ shell/sources/shell_exec.c
@@ -1,8 +1,14 @@
 #include "../headers/shell_sec.h"
-#include <unistd.h>
+
 #include <stdio.h>
 #include <stdbool.h>
 
+#ifdef _WIN32
+	#define popen _popen
+	#define pclose _pclose
+#else
+	#include <unistd.h>
+#endif
 
 int popenAction(char* cmd) {
 
