a68623a92b0d8dd45710431dbce262e9faad7819
cbdev <cb@cbcdn.com>
2014-03-06 16:38:44 +0100

    moved/refacted scancode helper

--- garfield-pos/scancode.c
+++ garfield-pos/scancode.c
@@ -7,32 +7,6 @@
 #include <linux/input.h>
 #include <unistd.h>
 
-char charFromKeycode(int kc){
-	switch(kc){
-		case KEY_1:
-			return '1';
-		case KEY_2:
-			return '2';
-		case KEY_3:
-			return '3';
-		case KEY_4:
-			return '4';
-		case KEY_5:
-			return '5';
-		case KEY_6:
-			return '6';
-		case KEY_7:
-			return '7';
-		case KEY_8:
-			return '8';
-		case KEY_9:
-			return '9';
-		case KEY_0:
-			return '0';
-	}
-	return 'x';
-}
-
 int main(int argc, char** argv){
 	char* input_device=NULL;
 	int fd, bytes;
@@ -66,7 +40,7 @@ int main(int argc, char** argv){
 			if(ev.value!=0){
 				continue;
 			}
-			printf("Value %d, Code %d, Type %d - %c\n",ev.value, ev.code, ev.type, charFromKeycode(ev.code));
+			printf("Scancode %d\n", ev.code);
 		}
 	}
 
